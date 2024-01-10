b7a4dba832c1f3d3fbc35487bc22ddfafaf6d23c
headchezze <gribnik2281337@gmail.com>
2024-01-10 22:42:22 +0500

    Initial commit

---
+++ FieldValidatorAPI/CommonFieldValidatorFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldValidatorAPI
+{
+    public class CommonFieldValidatorFunctions
+    {
+        public delegate bool RequiredValidDel(string fieldVal);
+        public delegate bool StringLengthValidDel(string fieldVal, int min, int max);
+        public delegate bool DateValidDel(string fieldVal, out DateTime validDateTime);
+        public delegate bool PatternMatchDel(string fieldVal, string pattern);
+        public delegate bool CompareFieldValidDel(string filedVal, string fieldValCompare);
+
+    }
+}
